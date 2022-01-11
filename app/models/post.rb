6ca0b8efd24cd06860d79b647cfcb0da9716578c
duneSpice97 <89211252+duneSpice97@users.noreply.github.com>
2022-01-11 10:28:06 -0700

    commit tree 5f323c27eb33c62f09491201fcc192ea82a04fff

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+    validates :title, presence: true, inclusion: {in: ("Won't Believe" "Secret" "Top [number]" "Guess")}
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+
+
 end
